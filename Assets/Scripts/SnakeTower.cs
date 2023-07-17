31810a4d30ad5d4c8e14d66cfc6b20e66c3c649e
czhang0003 <cindyzhang716@gmail.com>
2023-07-17 15:55:04 -0700

    SnakeTower
    TD Day 1

---
+++ Assets/Scripts/SnakeTower.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeTower : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    
+    //member variables
+
+    public GameObject spit;
+    public GameObject muzzle;
+    public Vector3 position = transform.position; 
+    public int viewRange = 20;
+    public int attackRange = 15;
+    public float fireRate = 2f;
+    public float ballSpeed = 1000f;
+
+
+    public enum SnakeState
+    {
+        None, FindTarget, Attack
+    }
+
+    public SnakeState state = SnakeState.None;
+    public bool enemyInRange;
+    public GameObject target;
+    public bool enemyInAttackRange;
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void enemyInAttackRange()
+    {
+        target = FindClosestTarget("Monkey", attackRange);
+
+        return target != null;
+
+    }
+
+    void CheckGameState()
+    {
+        lastFire += Time.deltaTime;
+
+        enemyInAttackRange = enemyInAttackRange();
+
+        if(enemyInAttackRange)
+        {
+            state = SnakeState.Attack;
+        }
+
+
+    }
+
+}
